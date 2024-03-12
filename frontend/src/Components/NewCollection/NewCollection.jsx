6047216196a3d45c1a1ddf69c8a6297423076496
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-03-12 18:13:32 +0545

    Update NewCollection.jsx

--- frontend/src/Components/NewCollection/NewCollection.jsx
+++ frontend/src/Components/NewCollection/NewCollection.jsx
@@ -1,23 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import './NewCollection.css'
- import Item  from '../Item/Item'
-  const NewCollection = () => {
-  const[new_collection,setnewcollection]=useState([])
-  useEffect(()=>{
-fetch('https://ec-backend-1.onrender.com/newcollection')
-.then((response)=>response.json())
-.then((data)=>setnewcollection(data))
-  },[])
+ 
+import React, { useEffect, useState } from 'react';
+import './NewCollection.css';
+import Item from '../Item/Item';
+
+const NewCollection = () => {
+  const [newCollection, setNewCollection] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ec-backend-1.onrender.com/newcollection')
+      .then(response => response.json())
+      .then(data => setNewCollection(data))
+      .catch(error => console.error('Error fetching new collection:', error));
+  }, []);
+
   return (
     <div className="NewCollection">
-        <h1>NEW COLLECTION</h1>
-        <hr/>
-        <div className="collections">
-        {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-            })}
-        </div>
+      <h1>NEW COLLECTION</h1>
+      <hr />
+      <div className="collections">
+        {newCollection.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={`https://ec-backend-1.onrender.com/images/${item.image}`} // Adjusted image URL
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
-export default NewCollection
+  );
+};
+
+export default NewCollection;
