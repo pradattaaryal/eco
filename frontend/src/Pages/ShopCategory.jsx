afc84ae7808affbe15d622d89dc03fea4b270745
pradattaaryal <128461960+pradattaaryal@users.noreply.github.com>
2024-03-13 06:41:32 +0545

    Update ShopCategory.jsx

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,37 +1,47 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import {ShopContext} from '../Context/Shopcontext'
-import drop from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
-  const ShopCategory = (props) => {
-    const {all_product}=useContext(ShopContext)
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/Shopcontext';
+import drop from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
+
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+
   return (
     <div className='shopcat'>
-      <img className='shopcatbanner' src={props.banner} alt="" />
-      <div className="shopcatindexsort">
+      <img className='shopcatbanner' src={props.banner} alt='' />
+      <div className='shopcatindexsort'>
         <div>
-        <p>
-          <span>showing 1-12</span> out of 36 product
-        </p>
+          <p>
+            <span>showing 1-12</span> out of 36 product
+          </p>
         </div>
-        <div className="shopcatsort">
-          sort by <img src={drop} alt="" />
+        <div className='shopcatsort'>
+          sort by <img src={drop} alt='' />
         </div>
       </div>
-      <div className="shopcat-product">
-        {all_product.map((item,i)=>{
-          if(props.category===item.category)
-          {
-            return <Item  key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} ></Item>
+      <div className='shopcat-product'>
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+             const imageUrl = `https://eco-backend-boxa.onrender.com/images/${item.image}`;
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={imageUrl}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
           }
-          else 
-          {return null;}
         })}
       </div>
-      <div className="loadm">
-        Explore
-      </div>
+      <div className='loadm'>Explore</div>
     </div>
-  )
-}
-export default ShopCategory 
+  );
+};
+
+export default ShopCategory;
